d7c6a3665f3c71c7a938908e83ec10946c769ee9
Ryan Leckey <leckey.ryan@gmail.com>
2017-09-08 02:40:08 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,43 @@
+var targets = {
+    "browsers": "last 2 versions, > 2%",
+};
+
+module.exports = function (context, options = {}) {
+    var debug = (options && typeof options.debug === 'boolean') ? !!options.debug : false;
+
+    return {
+        "presets": [
+            require('babel-preset-env').default(null, {
+                "debug": debug,
+                "modules": false,
+                "targets": targets,
+                "loose": true,
+                "targets": {
+                    "browsers": "last 2 versions, > 2%"
+                },
+                "modules": false,
+                "regenerator": true,
+                "useBuiltIns": true
+            }),
+            require('babel-preset-react')
+        ],
+        "plugins": [
+            require("babel-plugin-lodash"),
+        ],
+        "env": {
+            "development": {
+                "plugins": [require("react-hot-loader/babel")],
+            },
+            "production": {
+                "presets": [
+                    [
+                        require("babili").default(null, {
+                            removeConsole: true,
+                            removeDebugger: true,
+                        })
+                    ]
+                ]
+            }
+        }
+    }
+}
