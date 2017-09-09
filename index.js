30b87687e9f075f8a2cbe77e50918bbc0e17230c
Ryan Leckey <leckey.ryan@gmail.com>
2017-09-08 18:03:08 -0700

    Add stage-2

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var targets = {
-    "browsers": "last 2 versions, > 2%",
+  browsers: "last 2 versions, > 2%"
 };
 
-module.exports = function (context, options = {}) {
-    var debug = (options && typeof options.debug === 'boolean') ? !!options.debug : false;
+module.exports = function(context, options = {}) {
+  var debug =
+    options && typeof options.debug === "boolean" ? !!options.debug : false;
 
-    return {
-        "presets": [
-            require('babel-preset-env').default(null, {
-                "debug": debug,
-                "modules": false,
-                "targets": targets,
-                "loose": true,
-                "targets": {
-                    "browsers": "last 2 versions, > 2%"
-                },
-                "modules": false,
-                "regenerator": true,
-                "useBuiltIns": true
-            }),
-            require('babel-preset-react')
-        ],
-        "plugins": [
-            require("babel-plugin-lodash"),
-        ]
-    }
-}
+  return {
+    presets: [
+      require("babel-preset-env").default(null, {
+        debug: debug,
+        modules: false,
+        targets: targets,
+        loose: true,
+        targets: {
+          browsers: "last 2 versions, > 2%"
+        },
+        modules: false,
+        regenerator: true,
+        useBuiltIns: true
+      }),
+      require("babel-preset-stage-2"),
+      require("babel-preset-react")
+    ],
+    plugins: [require("babel-plugin-lodash")]
+  };
+};
